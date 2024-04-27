28f3dae51ee3b6a6e43e0f5d01679ffc01c4fc0f
Kynn274 <117021914+Kynn274@users.noreply.github.com>
2024-04-27 19:55:20 +0700

    Update TIMCHU_PUZZLE.cpp

--- TIMCHU_PUZZLE.cpp
+++ TIMCHU_PUZZLE.cpp
@@ -191,8 +191,8 @@ bool find_string_right(string s, long long m, long long n, vector<vector<char> >
       tmp2 = tmp1;
       reverse(tmp2.begin(), tmp2.end());
       if(s == tmp1 || s == tmp2){
-        _x = _left_x[i];
-        _y = _left_y[i];
+        _x = _right_x[i];
+        _y = _right_y[i];
         if(s == tmp1) chieu = 1;
         else chieu = -1;
         break;
